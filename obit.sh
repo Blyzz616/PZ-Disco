3fdc12fb6cfc52c22c7827eb3e8e82b0c97a2236
Jim <blyzzcanada@gmail.com>
2022-04-28 15:09:22 -0700

    Update obit.sh

--- obit.sh
+++ obit.sh
@@ -5,21 +5,21 @@ RED=16711680
 # Line 7 needs to contain your Webhook URL in full. Next line is an example
 #URL='https://discord.com/api/webhooks/931526008698012384/9PF5y2X8d55z064xaCavlXgMuF9cb9N42PrrNESrnIGc1L0znHyIZLfQRPFGPUtmyNAr'
 URL=''
-source /usr/local/bin/colours.lib
 
-    # Lets put in some funny death messages
-    RANDOS=('just died.' 'has now made ther contribution to the horde.' 'swapped sides.' 'has now completed their playthough.' 'used the wrong hole.' 'kicked the bucket.' 'decided to try something else (it did not work).' 'forgot to pay their tribute to the R-N-Geezus.' 'bought the farm.''is still walking... breathing... not so much' )
+# Lets put in some funny death messages
+# Credit where credit is due, I took inspiration from https://www.reddit.com/r/projectzomboid/comments/u3pivr/need_helpsuggestionswitty_comments/
+RANDOS=('just died.' 'has now made ther contribution to the horde.' 'swapped sides.' 'has now completed their playthough.' 'used the wrong hole.' 'kicked the bucket.' 'decided to try something else (it did not work).' 'forgot to pay their tribute to the R-N-Geezus.' 'bought the farm.''is still walking... breathing... not so much' )
 
-SURVEY(){
-VOYEUR=$(ls /root/Zomboid/Logs/ | grep 'user' | tail -n1)
-  tail -fn0 /root/Zomboid/Logs/$VOYEUR | \
-  while read LINE ; do
+OBITUARY(){
+USERFILE=$(ls /root/Zomboid/Logs/ | grep 'user' | tail -n1)
+  tail -fn0 "/root/Zomboid/Logs/$USERFILE" | \
+  while read -r LINE ; do
 
     # We're gonna need a seed
     RANDOM=$$$(date +%s)
 
-    DEADPLAYER=$(echo "$LINE" | egrep -o '\S+\sdied' | awk '{print $1}')
-    if [[ ! -z $DEADPLAYER ]];
+    DEADPLAYER=$(echo "$LINE" | grep -E -o '\S+\sdied' | awk '{print $1}')
+    if [[ -n $DEADPLAYER ]];
     then
       MESSAGE1=${RANDOS[ $RANDOM % ${#RANDOS[@]} ]}
       OBIT="_$(date +%H:%M):_ **$DEADPLAYER** $MESSAGE1"
@@ -30,24 +30,24 @@ VOYEUR=$(ls /root/Zomboid/Logs/ | grep 'user' | tail -n1)
     # test if the VOYEUR file is still the current log file
     if [[ $VOYEUR != $(ls /root/Zomboid/Logs/ | grep 'user' | tail -n1) ]];
     then
-      SURVEY
+      OBITUARY
     fi
   done
-SURVEY
+OBITUARY
 
 }
 
-TEST(){
+VALIDATE(){
   [[ $(find /root/Zomboid/Logs/ -maxdepth 1 -name '*user.txt' | wc -l) -eq 0 ]] && PRESENT=0 || PRESENT=1
 
   if [[ $PRESENT -eq 0 ]]
   then
     sleep 1
-    TEST
+    VALIDATE
   else
-    SURVEY
+    OBITUARY
   fi
 
 }
 
-TEST
+VALIDATE
